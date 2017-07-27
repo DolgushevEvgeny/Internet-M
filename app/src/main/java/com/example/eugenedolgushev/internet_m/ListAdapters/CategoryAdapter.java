ce9e9af33a92409da9f58cba57b5ff74e98ac8f9
Dolgushev Evgeny <baraxlush1995@gmail.com>
2017-07-27 11:40:17 +0300

    Доработки
    Адаптеры для списков

---
+++ app/src/main/java/com/example/eugenedolgushev/internet_m/ListAdapters/CategoryAdapter.java
@@ -0,0 +1,61 @@
+package com.example.eugenedolgushev.internet_m.ListAdapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eugenedolgushev.internet_m.CustomOnItemClickListener;
+import com.example.eugenedolgushev.internet_m.Model.Category;
+import com.example.eugenedolgushev.internet_m.R;
+import com.example.eugenedolgushev.internet_m.ViewHolders.CategoryViewHolder;
+
+import java.util.ArrayList;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryViewHolder>{
+
+    private ArrayList<Category> categories;
+    private CustomOnItemClickListener listener;
+
+    public CategoryAdapter(ArrayList<Category> categories, CustomOnItemClickListener listener) {
+        this.categories = categories;
+        this.listener = listener;
+    }
+
+    @Override
+    public CategoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item, parent, false);
+        final CategoryViewHolder mViewHolder = new CategoryViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(v, mViewHolder.getPosition());
+            }
+        });
+
+        return new CategoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CategoryViewHolder holder, int position) {
+        holder.setCategoryName(this.categories.get(position).getName());
+        holder.setCategoryImage(this.categories.get(position).getImageUrl());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.categories.size();
+    }
+
+    public Category getItem(int position) {
+        if (position >= 0 && position <= this.categories.size() - 1) {
+            return this.categories.get(position);
+        }
+        return null;
+    }
+
+    public void setList(ArrayList<Category> list) {
+        categories = list;
+        notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/example/eugenedolgushev/internet_m/ListAdapters/ProductAdapter.java
@@ -0,0 +1,62 @@
+package com.example.eugenedolgushev.internet_m.ListAdapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eugenedolgushev.internet_m.CustomOnItemClickListener;
+import com.example.eugenedolgushev.internet_m.Model.Product;
+import com.example.eugenedolgushev.internet_m.R;
+import com.example.eugenedolgushev.internet_m.ViewHolders.ProductViewHolder;
+
+import java.util.ArrayList;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductViewHolder> {
+
+    private ArrayList<Product> products;
+    private CustomOnItemClickListener listener;
+
+    public ProductAdapter(ArrayList<Product> products, CustomOnItemClickListener listener) {
+        this.products = products;
+        this.listener = listener;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item, parent, false);
+        final ProductViewHolder mViewHolder = new ProductViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(v, mViewHolder.getPosition());
+            }
+        });
+
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        holder.setProductName(this.products.get(position).getTitle());
+        holder.setProductPrice(this.products.get(position).getPrice());
+        holder.setProductImage(this.products.get(position).getImageUrl());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.products.size();
+    }
+
+    public Product getItem(int position) {
+        if (position >= 0 && position <= this.products.size() - 1) {
+            return this.products.get(position);
+        }
+        return null;
+    }
+
+    public void setList(ArrayList<Product> list) {
+        products = list;
+        notifyDataSetChanged();
+    }
+}
