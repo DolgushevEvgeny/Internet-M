01d6636b4edcefff0508267462610270d6d450fc
Dolgushev Evgeny <baraxlush1995@gmail.com>
2017-08-04 20:41:42 +0300

    Добавлена активность

---
+++ app/src/main/java/com/example/eugenedolgushev/internet_m/Activities/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.eugenedolgushev.internet_m.Activities;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.eugenedolgushev.internet_m.Fragments.CategoryFragment;
+import com.example.eugenedolgushev.internet_m.Fragments.ProductFragment;
+import com.example.eugenedolgushev.internet_m.R;
+import com.example.eugenedolgushev.internet_m.Fragments.TransferData;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, TransferData {
+
+    private CategoryFragment categoryFragment;
+    private ProductFragment productFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        productFragment = new ProductFragment();
+        productFragment.setContext(this);
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_wrapper, productFragment, "productFragment");
+        fragmentTransaction.commit();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.products) {
+            productFragment = new ProductFragment();
+            productFragment.setContext(this);
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.remove(categoryFragment);
+            fragmentTransaction.add(R.id.fragment_wrapper, productFragment, "productFragment");
+            fragmentTransaction.commit();
+        } else if (id == R.id.categories) {
+            categoryFragment = new CategoryFragment();
+            categoryFragment.setContext(this);
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.remove(productFragment);
+            fragmentTransaction.add(R.id.fragment_wrapper, categoryFragment, "categoryFragment");
+            fragmentTransaction.commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void sendData(Bundle bundle) {
+        productFragment = new ProductFragment();
+        productFragment.setContext(this);
+        productFragment.setArguments(bundle);
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.remove(categoryFragment);
+        fragmentTransaction.add(R.id.fragment_wrapper, productFragment, "productFragment");
+        fragmentTransaction.commit();
+    }
+}
